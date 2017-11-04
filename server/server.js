0a5d1ca8a6c5076b91748244af2017c3a6a18aa8
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2017-11-04 15:15:43 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -22,13 +23,27 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-	Todo.find().then((todos) => {
-		res.send({todos});
-	}, (e) => {
-		res.status(400).send(e);
-	});
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((e) => res.status(400).send());
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
