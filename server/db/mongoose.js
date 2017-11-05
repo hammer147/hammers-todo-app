ee811cdb33c2289572680cb468cc73d33f08d472
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2017-11-05 15:29:06 +0100

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // use es6 promises
 
-const uri = 'mongodb://localhost:27017/TodoApp';
+// const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const uri = 'mongodb://Daenerys:Targaryen@ds249355.mlab.com:49355/todo-api';
+
 const options = { useMongoClient: true }; // needed in mongoose >= 4.11.0
 mongoose.connect(uri, options);
 
